4d5705fd86799bb553f496fa3d4ef83173dc9e02
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-07-12 23:44:08 +0530

    LeetCode SQL 50 Solutions

---
+++ Basic_Joins/Replace_Employee_ID_The_Unique_Identifier.sql
@@ -0,0 +1,26 @@
+/*
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+ Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+*/
+
+SELECT   U.unique_id,E.name FROM Employees as E LEFT JOIN EmployeeUNI as U ON E.id=U.id;
